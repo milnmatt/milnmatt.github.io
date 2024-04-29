aa503ba21642231478737eecc6010a31e157d366
Matthew Milne <matthew@teammilne.com>
2024-04-28 21:53:48 -0400

    Push to main

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('startButton');
     // Retrieve the 'Learn More' button using its ID
     const learnMoreButton = document.getElementById('learnMoreButton');
-
+    // Retrieve the 'Rise' button using its ID
+    const riseButton = document.getElementById('riseButton');
+    // Retrieve the 'fake' button using its ID
+    const fakeButton = document.getElementById('fakeButton');
     // Add a click event listener to the 'Start Your Adventure' button
     startButton.addEventListener('click', function() {
-        // Redirect the user to the game page when the button is clicked
+    // Redirect the user to the game page when the button is clicked
         window.location.href = 'game.html';
     });
-
     // Add a click event listener to the 'Learn More' button
     learnMoreButton.addEventListener('click', function() {
         // Redirect the user to the about page when the button is clicked
         window.location.href = 'about.html';
+    });
+    // Add a click event listener to the 'rise' button
+    riseButton.addEventListener('click', function() {
+        // Redirect the user to the about page when the button is clicked
+        window.location.href = 'game2.html';
+    });
+    // Add a click event listener to the 'fake' button
+    fakeButton.addEventListener('click', function() {
+        // Redirect the user to the about page when the button is clicked
+        window.location.href = 'gameover.html';
     });
 });
